b44559a5aae404027a350aa1b4d02775cc60aff0
Jay Waddy <40751612+JayWaddy@users.noreply.github.com>
2019-09-13 20:08:41 -0400

    Dictionary objects, not arays

--- js/hayo.js
+++ js/hayo.js
@@ -1,11 +1,14 @@
+// Dictionary
 const App = {
 
     word: [
         // H
-        [ 'hello', 'hayo' ],
+        { eng: 'hello', plc: 'hayo' },
+
         // W
-        [ 'world', 'wuntow' ]
+        { eng: 'world', plc: 'wuntow' }
     ]
+    
 };
 
 const input = document.querySelector('input');
@@ -13,28 +16,44 @@ const submit = document.querySelector('.submit');
 const output = document.querySelector('.output');
 
 submit.addEventListener('click', function() {
-    console.log(concat());
+
+    concat();
+
 });
 
 function concat() {
+
     let array = [];
     let str = '';
 
-    for (let i = 0; i < input.value.split(' ').length; i++) {
+    // Loop through input array
+    for ( let i = 0; i < input.value.split(' ').length; i++ ) {
+
+        // Loop through dictionary to match object values to array values
+        for ( let j = 0; j < App.word.length; j++ ) {
+
+            str += `${array[i] = App.word[j].eng} `;
+
+            if ( App.word[j].eng === array[i] ) {
+                
+                console.log('TRUE');
+                str += `${array[i] = App.word[j][1]} `;
+
+            } else {
+
+                // Unidentified words
+                // console.log('UNIDENTIFIED SCRIPT');
+
+            }
+
+        } // End of dictionary loop
+
         // Stores each word from userInput in an array 
         array.push(input.value.split(' ')[i].toLowerCase());
         
-        // Find equivilant in dicionary
         
-        str += `${array[i] = App.word[i][1]} `;
+    } // End of array loop
 
-        // Unidentified words
-        if ( array[i] !== App.word[i][0] ) {
-            console.log('NaW');
-        }
-    }
     return output.textContent = str;
-}
-// let grab = App.word.findIndex(function() {
-//     App.word.english === 'hello'
-// });
+
+} // End of concat()
