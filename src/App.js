b7bc5220cf90764fd96428c31d9d915730de5684
Jay Waddy <40751612+JayWaddy@users.noreply.github.com>
2021-08-22 16:57:11 -0400

    Init error fixes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-// Styles
-import GlobalStyle, { AppContainer } from "./helpers/styles/GlobalStyle";
-
-// Pages
-import Translate from "./pages/translate/Translate";
-import Glossary from "./pages/glossary/Glossary";
-
-// Components
-	// Navigation
-
-export default function App() {
-	return (
-    <>
-		<GlobalStyle/>
-		<AppContainer className="light-theme">
-			<Router>
-			{/* Header */}
-			<Switch>
-				<Route exact path="/" component={Translate}/>
-				<Route exact path="/glossary" component={Glossary}/>
-			</Switch>
-			{/* Navigation */}
-			</Router>
-		</AppContainer>
-    </>
-	);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
