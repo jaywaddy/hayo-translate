be5463db478f013fb1f9b20e9fbbb942d5135d00
Jay Waddy <40751612+JayWaddy@users.noreply.github.com>
2022-06-17 13:32:23 -0400

    Input Case Matching
    Input now matches the casing of the user input, but there is an error for using bas state.

--- src/pages/translate/Translate.jsx
+++ src/pages/translate/Translate.jsx
@@ -35,25 +35,50 @@ export default function Translate() {
         let outputString = "";
 
         // Iterate through user input to isolate earch term
-        userInput.map(input => {
-            const inputText = input.toLowerCase();
+        userInput.map(inputTerm => {
+            const matchCase = (planco, inputString = null) => {
+                let output = "";
+
+                // Return the case values for each character for every term
+                const getCase = target => {
+                    let cases = new Array(0);
+
+                    target.split("").map(character => {
+                        return character === character.toLowerCase()
+                            ? cases.push(0)  // Lowercase characters
+                            : cases.push(1); // Uppercase characters;
+                    });
+                    
+                    return cases;
+                }
+
+                const matchCaseValue = getCase(inputString);
+
+                // Create a new planco string with matching case values
+                planco.split("").forEach((character, index) => {
+                    matchCaseValue[index] === 1
+                        ? output += character.toUpperCase()
+                        : output += character;
+                });
+
+                return output;
+            }
+
             let counter = 0;
 
-            // Iterate through Data to see if terms match
+            // Iterate through Data to see if any terms match
             return Data.map(entry => {
                 const match = entry.eng.toLowerCase();
                 const planco = entry.plc;
 
-                // If a match has been found, setOutput to term
-                inputText === match
-                    ? outputString += planco
-                    : inputText === /\W/
-                        ? outputString += planco
-                        : counter++;
+                // If a match has been found, setOutput to planco term
+                inputTerm.toLowerCase() === match || inputTerm === /\W/
+                    ? outputString += matchCase(planco, inputTerm)
+                    : counter++;
 
-                // If there are no matches, return the input
+                // If there are no matches, return the user input
                 return counter === Data.length
-                    ? outputString += inputText
+                    ? outputString += inputTerm
                     : null
             });
         });
